29c231ff15a8a8c973e3d0abe54cb9ed6adc8029
Thomas Dufourd <thomas.dufourd@bekk.no>
2015-11-06 11:29:43 +0100

    [coding-dojo] ready to start

--- dojo/main.go
+++ dojo/main.go
@@ -1,33 +1,10 @@
 package main
-import "fmt"
 
 func main() {
-	addOnePointToThisScore(0, 0)
+	methodUnderTest()
 }
 
-const Win = -1
-
-type scoreboard struct {
-	player1 int
-	player2 int
-}
-
-func (s *scoreboard) ScorePlayer1() (p1, p2 int) {
-
-}
-
-func (s *scoreboard) ScorePlayer2() (p1, p2 int) {
-
-}
-func addOnePointToThisScore(score1, score2 int) (p1, p2 int) {
-	updatedScore := score1 + 15
-	if score1 == 30 {
-		return 40, score2
-	}
-	if score1 == 40 {
-		return Win, score2
-	}
-	fmt.Printf("Will now add 1 point to this score '%v'. The new score is '%v'", score1, updatedScore)
-	return updatedScore, score2
+func methodUnderTest() (string) {
+	return "it works!"
 }
 
--- dojo/main_test.go
+++ dojo/main_test.go
@@ -4,65 +4,10 @@ import (
 	"testing"
 )
 
-func TestTennisScore15(t *testing.T) {
-	expected := 30
+func TestAMethod(t *testing.T) {
+	expected := "Does it work?"
 	// call the method under test
-	result, _ := addOnePointToThisScore(15, 0)
-
-	if expected != result {
-		t.Errorf("error expected '%v' but was '%v'", expected, result)
-	}
-}
-
-func TestTennisScore0(t *testing.T) {
-	expected := 15
-
-	// call the method under test
-	result, _ := addOnePointToThisScore(0, 0)
-
-	if expected != result {
-		t.Errorf("error expected '%v' but was '%v'", expected, result)
-	}
-
-}
-
-func TestTennisScore30(t *testing.T) {
-	expected := 40
-
-	// call the method under test
-	result, _ := addOnePointToThisScore(30, 0)
-
-	if expected != result {
-		t.Errorf("error expected '%v' but was '%v'", expected, result)
-	}
-}
-
-func TestTennisScore40(t *testing.T) {
-	expected := Win
-
-	// call the method under test
-	result, _ := addOnePointToThisScore(40, 0)
-
-	if expected != result {
-		t.Errorf("error expected '%v' but was '%v'", expected, result)
-	}
-
-}
-
-func TestTennisScore0Player2(t *testing.T) {
-	expected := 15
-	// call the method under test
-	_, result := addOnePointToThisScore(0, 0)
-
-	if expected != result {
-		t.Errorf("error expected '%v' but was '%v'", expected, result)
-	}
-}
-
-func TestTennisScore15Player2(t *testing.T) {
-	expected := 30
-	// call the method under test
-	_, result := addOnePointToThisScore(0, 15)
+	result:= methodUnderTest()
 
 	if expected != result {
 		t.Errorf("error expected '%v' but was '%v'", expected, result)
