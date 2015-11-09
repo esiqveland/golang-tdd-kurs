757d2c3619750ee724de20d8c42cb3a98e08832b
Thomas Dufourd <thomas.dufourd@bekk.no>
2015-11-09 08:56:47 +0100

    [dojo] work done during the coding dojo

--- dojo/main.go
+++ dojo/main.go
@@ -1,10 +1,124 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
 func main() {
-	methodUnderTest()
+	wg.Add(1)
+
+	game := New()
+	c := game.Scores()
+
+	go printGame(4, c)
+
+	game.ScorePlayer1()
+	game.ScorePlayer1()
+	game.ScorePlayer2()
+	game.ScorePlayer2()
+	game.ScorePlayer1()
+
+	wg.Wait()
 }
 
-func methodUnderTest() (string) {
-	return "it works!"
+func printGame(i int, c chan game) {
+	count := 0
+	for g := range c {
+		fmt.Printf("[logstate] %v\n", g)
+		count = count + 1
+		if count == i {
+			wg.Done()
+		}
+	}
+}
+func New() TennisScorer {
+	return &game{
+		player1:  0,
+		player2:  0,
+		producer: make(chan game),
+	}
 }
 
+type game struct {
+	player1  int
+	player2  int
+	winner   string
+	producer chan game
+}
+
+const Wins = -1
+const Adv = -2
+
+type TennisScorer interface {
+	ScorePlayer1()
+	ScorePlayer2()
+	Winner() string
+	Scores() chan game
+	String() string
+}
+
+func (g *game) String() string {
+	return fmt.Sprintf("%v %v", g.player1, g.player2)
+}
+func (g *game) Winner() string {
+	return g.winner
+}
+func (g *game) logState() {
+	newState := *g
+	g.producer <- newState
+}
+
+func (game *game) Scores() chan game {
+	return game.producer
+}
+
+func (game *game) checkWinner() {
+	if game.player1 == Wins {
+		game.winner = "player1"
+	}
+	if game.player2 == Wins {
+		game.winner = "player2"
+	}
+}
+
+func (game *game) ScorePlayer1() {
+	game.player1, game.player2 = ScorePlayer(game.player1, game.player2)
+	game.checkWinner()
+	game.logState()
+}
+
+func (game *game) ScorePlayer2() {
+	game.player2, game.player1 = ScorePlayer(game.player2, game.player1)
+	game.checkWinner()
+	game.logState()
+}
+
+func ScorePlayer(player int, otherPlayer int) (int, int) {
+	switch player {
+	case 0:
+		player = 15
+	case 15:
+		player = 30
+	case 30:
+		player = 40
+	case 40:
+		if otherPlayer == 40 {
+			player = Adv
+		} else if otherPlayer == Adv {
+			otherPlayer = 40
+		} else {
+			player = Wins
+		}
+	case Adv:
+		player = Wins
+	}
+
+	return player, otherPlayer
+}
+
+func newGame() game {
+	return game{}
+}
--- dojo/main_test.go
+++ dojo/main_test.go
@@ -4,12 +4,170 @@ import (
 	"testing"
 )
 
-func TestAMethod(t *testing.T) {
-	expected := "Does it work?"
-	// call the method under test
-	result:= methodUnderTest()
+
+func TestPlayer1ScoreTheFirstPoint(t *testing.T) {
+	expected := game{player1: 15, player2: 0}
+	game := game{}
+	game.ScorePlayer1()
+
+	testPlayerScores(t, game, expected)
+}
+
+func assert(t *testing.T, expected string, actual string) {
+	if expected != actual {
+		t.Errorf("error expected '%v' but was '%v'", expected, actual)
+	}
+}
+
+func testPlayerScores(t *testing.T, expected, actual game) {
+	if expected.player1 != actual.player1 {
+		t.Errorf("error expected player1 '%v' but was '%v'", expected, actual)
+	}
+	if expected.player2 != actual.player2 {
+		t.Errorf("error expected player2 '%v' but was '%v'", expected, actual)
+	}
+}
+
+func TestPlayer1ScoresFrom15(t *testing.T) {
+	expected := game{player1: 30}
+
+	game := game{player1:15}
+	game.ScorePlayer1()
+
+	testPlayerScores(t, expected, game)
+}
+
+func TestPlayer1ScoresFrom30(t *testing.T) {
+	expected := game{player1: 40}
+
+	game := game{player1:30}
+	game.ScorePlayer1()
+
+	testPlayerScores(t, expected, game)
+}
+
+func TestPlayer2ScoresFrom30(t *testing.T) {
+	expected := game{player2: 40}
+
+	game := game{player2:30}
+	game.ScorePlayer2()
+
+	testPlayerScores(t, expected, game)
+}
+
+func TestPlayer2WinsTheGameIfScoresFrom40(t *testing.T) {
+	game := game{player2: 40}
+	expected := "player2"
+
+	game.ScorePlayer2()
+
+	actual := game.winner
+	assert(t, expected, actual)
+}
+
+func TestGivenPlayer1AndPlayer2HasScore40_WhenPlayer1Score_ThenNoWinner(t *testing.T) {
+	game := game{player2: 40, player1: 40}
+	expected := ""
+
+	game.ScorePlayer1()
+
+	actual := game.winner
+	assert(t, expected, actual)
+}
+
+func TestGivenPlayer1AndPlayer2HasScore40_WhenPlayer1Scores_ThenPlayer1GetsAdv(t *testing.T) {
+	setup := game{player1: 40, player2: 40}
+	expected := game{player1: Adv, player2: 40}
+
+	setup.ScorePlayer1()
+
+	testPlayerScores(t, expected, setup)
+}
+
+func TestGivenPlayer1AndPlayer2HasScore40_WhenPlayer2Scores_ThenPlayer2GetsAdv(t *testing.T) {
+	setup := game{player1: 40, player2: 40}
+	expected := game{player1: 40, player2: Adv}
+
+	setup.ScorePlayer2()
+
+	testPlayerScores(t, expected, setup)
+}
+
+func TestGivenPlayer1AndPlayer2HasScore40_WhenPlayer2ScoresTwice_ThenPlayer2Wins(t *testing.T) {
+	setup := game{player1: 40, player2: 40}
+	expected := game{player1: 40, player2: Wins}
+
+	setup.ScorePlayer2()
+	setup.ScorePlayer2()
+
+	testPlayerScores(t, expected, setup)
+}
+
+func TestGivenPlayer1AndPlayer2HasScore40_WhenPlayer2Score_ThenNoWinner(t *testing.T) {
+	game := game{player2: 40, player1: 40}
+	expected := ""
+
+	game.ScorePlayer2()
+
+	actual := game.winner
+	assert(t, expected, actual)
+}
+
+func TestPlayer2DoesntWinTheGameIfScoresNotFrom40(t *testing.T) {
+	game := game{player2: 30}
+	expected := ""
+
+	game.ScorePlayer2()
+
+	actual := game.winner
+	assert(t, expected, actual)
+}
+
+
+func TestPlayer1WinsTheGameIfScoresFrom40(t *testing.T) {
+	game := game{player1: 40}
+	expected := "player1"
+
+	game.ScorePlayer1()
+
+	actual := game.winner
+	assert(t, expected, actual)
+}
+
+func TestPlayer1Does_NotWinTheGameIf_ScoresFrom30(t *testing.T) {
+	game := game{player1: 30}
+	expected := ""
+
+	game.ScorePlayer1()
+
+	actual := game.winner
+	assert(t, expected, actual)
+}
+
+func TestGivenPlayer1HasAdvantage_WhenPlayer2Scores_ThenPlayer1AndPlayer2Has40(t *testing.T) {
+	setup := game{player1: Adv, player2: 40}
+	expected := game{player1: 40, player2: 40}
+
+	setup.ScorePlayer2()
+
+	testPlayerScores(t, expected, setup)
+}
+
+func TestGivenPlayer2HasAdvantage_WhenPlayer1Scores_ThenPlayer1AndPlayer2Has40(t *testing.T) {
+	setup := game{player1: 40, player2: Adv}
+	expected := game{player1: 40, player2: 40}
+
+	setup.ScorePlayer1()
+
+	testPlayerScores(t, expected, setup)
+}
+
+func TestStartMatch(t *testing.T) {
+	expected := game{player1:0, player2:0}
+	result := newGame()
 
 	if expected != result {
 		t.Errorf("error expected '%v' but was '%v'", expected, result)
 	}
 }
+
